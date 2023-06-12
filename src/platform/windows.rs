fa4e0be6c1088ed327d34f7bab69cf4ab7f30ee1
Lucas Clemente Vella <lvella@gmail.com>
2023-06-13 00:06:34 +0100

    Tentative test for Windows fallocate-like function.

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -18,6 +18,8 @@ use windows_sys::Win32::{
     },
 };
 
+const ENTRY_SIZE: u32 = std::mem::size_of::<FILE_ALLOCATED_RANGE_BUFFER>() as u32;
+
 lazy_static! {
     /// The page size.
     pub static ref PAGESIZE: u64 = {
@@ -49,8 +51,6 @@ pub fn preallocate_file(file: &mut File, size: u64) -> Result<(), Error> {
 }
 
 fn mimic_fallocate(file: &mut File, size: u64) -> Result<(), Error> {
-    const ENTRY_SIZE: u32 = std::mem::size_of::<FILE_ALLOCATED_RANGE_BUFFER>() as u32;
-
     // To be sure, seek the file to the beginning:
     let mut file_pos = file.seek(io::SeekFrom::Start(0))?;
 
@@ -66,7 +66,7 @@ fn mimic_fallocate(file: &mut File, size: u64) -> Result<(), Error> {
             let success = DeviceIoControl(
                 file.as_raw_handle() as HANDLE,
                 FSCTL_QUERY_ALLOCATED_RANGES,
-                query_range.as_mut_ptr() as *mut c_void,
+                query_range.as_ptr() as *const c_void,
                 ENTRY_SIZE,
                 allocated_slices.as_mut_ptr() as *mut c_void,
                 // Leave room in the output array so we can fill a dummy EOF
@@ -153,8 +153,131 @@ fn mimic_fallocate(file: &mut File, size: u64) -> Result<(), Error> {
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        env::temp_dir,
+        ffi::c_void,
+        fs::OpenOptions,
+        mem::{size_of_val, MaybeUninit},
+        os::windows::prelude::{AsRawHandle, FileExt},
+        ptr,
+    };
+
+    use windows_sys::Win32::{
+        Foundation::HANDLE,
+        System::{
+            Ioctl::{FILE_ALLOCATED_RANGE_BUFFER, FSCTL_QUERY_ALLOCATED_RANGES, FSCTL_SET_SPARSE},
+            IO::DeviceIoControl,
+        },
+    };
+
+    use crate::platform::windows::{mimic_fallocate, ENTRY_SIZE};
+
     #[test]
     fn test_mimic_fallocate() {
-        todo!()
+        let path = temp_dir().join("test-sparse-file.bin");
+        println!("File path: {path:?}");
+        let mut file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .open(path)
+            .unwrap();
+
+        // Set the file to sparse
+        unsafe {
+            let mut bytes_returned = 0u32;
+            assert!(
+                DeviceIoControl(
+                    file.as_raw_handle() as HANDLE,
+                    FSCTL_SET_SPARSE,
+                    ptr::null(),
+                    0,
+                    ptr::null_mut(),
+                    0,
+                    &mut bytes_returned,
+                    ptr::null_mut(),
+                ) != 0
+            );
+        }
+
+        let size = 50 * 1024 * 1024;
+        file.set_len(size).unwrap();
+
+        // Write to 3 equidistant places in the file:
+        let write_positions = [size * 1 / 4, size * 2 / 4, size * 3 / 4];
+        for pos in write_positions {
+            file.seek_write(&[42], pos).unwrap();
+        }
+
+        // Ensure the file has only 3 small allocated chunks:
+        unsafe {
+            let query_range = FILE_ALLOCATED_RANGE_BUFFER {
+                FileOffset: 0,
+                Length: size as i64,
+            };
+
+            let mut ranges: MaybeUninit<[FILE_ALLOCATED_RANGE_BUFFER; 3]> = MaybeUninit::uninit();
+
+            let mut ret_size = 0;
+            assert!(
+                DeviceIoControl(
+                    file.as_raw_handle() as HANDLE,
+                    FSCTL_QUERY_ALLOCATED_RANGES,
+                    ptr::addr_of!(query_range) as *const c_void,
+                    ENTRY_SIZE,
+                    ranges.as_mut_ptr() as *mut c_void,
+                    size_of_val(&ranges) as u32,
+                    &mut ret_size,
+                    ptr::null_mut(),
+                ) != 0
+            );
+
+            assert_eq!(ret_size as usize, size_of_val(&ranges));
+            for (r, expected_pos) in ranges.assume_init().into_iter().zip(write_positions) {
+                assert!(expected_pos >= r.FileOffset as u64);
+                assert!(expected_pos < (r.FileOffset + r.Length) as u64);
+                assert!(size > r.Length as u64);
+            }
+        }
+
+        // Fully allocate space for the file:
+        mimic_fallocate(&mut file, size).unwrap();
+
+        // Ensure the file's allocated chunks cover the entire size:
+        unsafe {
+            let mut cursor = 0u64;
+            while cursor < size {
+                let query_range = FILE_ALLOCATED_RANGE_BUFFER {
+                    FileOffset: cursor as i64,
+                    Length: (size - cursor) as i64,
+                };
+
+                let mut ranges = [MaybeUninit::<FILE_ALLOCATED_RANGE_BUFFER>::uninit(); 512];
+
+                let mut ret_size = 0;
+                assert!(
+                    DeviceIoControl(
+                        file.as_raw_handle() as HANDLE,
+                        FSCTL_QUERY_ALLOCATED_RANGES,
+                        ptr::addr_of!(query_range) as *const c_void,
+                        ENTRY_SIZE,
+                        ranges.as_mut_ptr() as *mut c_void,
+                        size_of_val(&ranges) as u32,
+                        &mut ret_size,
+                        ptr::null_mut(),
+                    ) != 0
+                );
+
+                assert!(ret_size > 0);
+                assert_eq!(ret_size % ENTRY_SIZE, 0);
+                let ranges = &ranges[..(ret_size / ENTRY_SIZE) as usize];
+                for r in ranges.into_iter().map(|r| r.assume_init_ref()) {
+                    assert_eq!(cursor, r.FileOffset as u64);
+                    cursor += r.Length as u64;
+                }
+            }
+            assert_eq!(cursor, size);
+        }
     }
 }
